b745f9d444fa994bf427fb23a9f41b8ce3647244
BorisDundakov <boris.stefan.de@gmail.com>
2023-01-04 14:58:31 +0200

    how to display date&time

---
+++ PS1.sh
@@ -0,0 +1,7 @@
+#PS1 -> primary prompt command --> what you see before typing a command in terminal
+
+#It is used to change the looks and environment of the shell command prompt
+
+
+export PS1="[\d]\n\u@\h: $ "
+# the command above displays the date and time
